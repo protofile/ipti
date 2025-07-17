fa962da195fddc2de3e44652c3672191c7d3bb82
yudantoardi <yudantoardi@gmail.com>
2025-07-17 21:49:41 +0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,57 @@
 $(document).ready(function(){
-  
+  $(".hero-slider").owlCarousel({
+        items: 1,
+        nav: true,
+        autplay: true,
+        loop: true
+        });
+
+        $(".testimonial-slider").owlCarousel({
+        items: 3,
+        nav: true,
+        dots: false,
+        autplay: true,
+        margin: 20,
+        loop: true,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480 : {
+                items: 1
+            },
+            // breakpoint from 768 up
+            768 : {
+                items: 3
+            },
+            // breakpoint from 768 up
+            1280 : {
+                items: 3
+            }
+        }
+    });
+
+    var docEl = $(document),
+        headerEl = $('header'),
+        navEl = $('nav'),
+        linkScroll = $('nav a');
+    
+    linkScroll.click(function(e){
+        e.preventDefault();
+
+        linkScroll.removeClass("active");
+        $(this).addClass("active");
+
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
+
+    $(".burger button").click(function(b){
+        b.preventDefault();
+        $("nav").toggleClass("show");
+        $(this).children("i").toggleClass("ri-menu-line ri-close-line");
+    });
 });
